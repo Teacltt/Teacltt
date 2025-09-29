a47fbfa6b88e3f018da12ae7ac14f5af68b2e7ba
Tea <tcloudlet@gmail.com>
2025-09-29 22:01:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-first_num=int(input('Введите первое число: '))
-second_num=int(input('Ввеите второе число: '))
-third_num=int(input('Ввеите третье число: '))
-list=[]
-list.append(first_num)
-list.append(second_num)
-list.append(third_num)
-print(max(list))
+month=int(input('Введите месяц: '))
+day=int(input('Введите день: '))
+if month <=2 or month == 12:
+    if month==2 and day <=28:
+            print('Это зима.')
+    elif month <2 or month == 12 and day <=31:
+            print ('Это зима.')
+    else:
+            print('Неверная дата')
+elif month <=5 and month >=3:
+    if day <=31:
+            print('Это весна')
+    else:
+            print('Неверная дата')
+elif month <=8 and month >=6:
+    if day <=31:
+            print('Это лето')
+    else:
+            print('Неверная дата')
+elif month <=11 and month >=9:
+    if month == 11 and day <31:
+            print('Это осень')
+    elif month <11 and month >=9 and day <=31:
+            print('Это осень')
+    else:
+            print('Неверная дата')
+else:
+    print('Неверная дата')
