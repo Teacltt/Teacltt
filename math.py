602a319860d507faf68578c33ccbcee4a52f195d
Tea <tcloudlet@gmail.com>
2025-09-29 22:02:34 +0300

    Create math.py

---
+++ math.py
@@ -0,0 +1,8 @@
+first_num=int(input('Введите первое число: '))
+second_num=int(input('Ввеите второе число: '))
+third_num=int(input('Ввеите третье число: '))
+list=[]
+list.append(first_num)
+list.append(second_num)
+list.append(third_num)
+print(max(list))
